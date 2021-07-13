9b9a864f666bb6619ea8ac3a7a04caad5065df38
Robie <robie.gonzales@dal.ca>
2021-07-13 19:10:45 -0300

    feat(web): show filtered results

--- lib/manga2gether_web/live/dashboard_live/search_component.ex
+++ lib/manga2gether_web/live/dashboard_live/search_component.ex
@@ -10,8 +10,30 @@ defmodule Manga2getherWeb.DashboardLive.SearchComponent do
         true
       end
 
+    # filter results
+    filtered_results =
+      Enum.filter(
+        socket.assigns.rooms,
+        fn room ->
+          current_manga = if room.current_manga == nil, do: ""
+
+          # show room if name, owners name, current manga or room code contain query value
+          String.downcase(room.name) =~ String.downcase(query) ||
+            String.downcase(room.user.username) =~ String.downcase(query) ||
+            String.downcase(current_manga) =~ String.downcase(query) ||
+            Integer.to_string(room.room_code) == String.downcase(query)
+        end
+      )
+
+    IO.inspect(filtered_results)
+
     {:noreply,
      socket
-     |> assign(:query, val)}
+     |> assign(:query, val)
+     |> assign(:results, filtered_results)}
+  end
+
+  def handle_event("join_room", %{"code" => room_code} = _params, socket) do
+    {:noreply, push_redirect(socket, to: Routes.room_show_path(socket, :show, room_code))}
   end
 end
